b0546e24ae8805fcecb6a8f2dc8ec4a018123bc3
PollyMorphism <skaletsky2014@gmail.com>
2024-05-29 21:19:51 +0400

    Add rubocop and rename app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WordsApp
+module TasksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
