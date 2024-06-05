e25251e96401688542372b9ab99ac29daa78fe0a
PollyMorphism <skaletsky2014@gmail.com>
2024-06-05 19:13:08 +0400

    Update rails to 7.1.3.4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_03_111220) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_05_151016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
