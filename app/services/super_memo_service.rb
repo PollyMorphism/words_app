a87457bba68e00dab9c6963a63f11b460666c77d
PollyMorphism <skaletsky2014@gmail.com>
2024-07-03 17:37:09 +0400

    Refactor SuperMemoService

--- app/services/super_memo_service.rb
+++ app/services/super_memo_service.rb
@@ -1,46 +1,54 @@
 # frozen_string_literal: true
 
 class SuperMemoService < ApplicationService
-  attr_accessor :card, :quality, :repetitions, :ease_factor, :interval
+  attr_reader :card, :quality
+
+  MIN_EASE_FACTOR = 1.3
 
   def initialize(card, quality)
     @card = card
     @quality = quality
-    @repetitions = card.repetitions
-    @ease_factor = card.ease_factor
-    @interval = card.interval
   end
 
   def call
-    if @quality >= 3
-      @interval = case @repetitions
-                  when 0
-                    1
-                  when 1
-                    3
-                  else
-                    (@interval * @ease_factor).ceil
-                  end
+    repetitions, ease_factor, interval = calculate_new_stats
 
-      @repetitions += 1
-    else
-      @repetitions = 0
-      @interval = 1
-    end
-    @ease_factor = new_ease_factor
-    @ease_factor = 1.3 if @ease_factor < 1.3
-
-    update_card
+    card.update!(repetitions:, ease_factor:, interval:, review_at: DateTime.now + interval.days)
   end
 
   private
 
-  def new_ease_factor
-    @ease_factor + (0.1 - (5 - @quality) * (0.08 + (5 - @quality) * 0.02))
+  def calculate_new_stats
+    repetitions = card.repetitions
+    ease_factor = card.ease_factor
+    interval    = card.interval
+
+    if quality >= 3
+      interval = calculate_new_interval(repetitions, interval, ease_factor)
+      repetitions += 1
+    else
+      repetitions = 0
+      interval = 1
+    end
+
+    ease_factor = calculate_new_ease_factor(ease_factor, quality)
+    ease_factor = MIN_EASE_FACTOR if ease_factor < MIN_EASE_FACTOR
+
+    [repetitions, ease_factor, interval]
   end
 
-  def update_card
-    review_at = DateTime.now + @interval.days
-    @card.update(repetitions: @repetitions, ease_factor: @ease_factor, interval: @interval, review_at:)
+  def calculate_new_interval(repetitions, prev_interval, ease_factor)
+    case repetitions
+    when 0
+      1
+    when 1
+      3
+    else
+      (prev_interval * ease_factor).ceil
+    end
+  end
+
+  def calculate_new_ease_factor(prev_factor, quality)
+    prev_factor + (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02))
   end
 end
